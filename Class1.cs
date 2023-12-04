3ff825e7cd4f61f6d8db6f4e69dedf5e451c2e60
w67184 <makssocha96@gmail.com>
2023-12-04 18:15:04 +0100

    Dodaj pliki projektów.

---
+++ Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Licz
+{
+    private int value;
+
+    public Licz(int startValue)
+    {
+        this.value = startValue;
+    }
+    public void dodaj(int variable)
+    {
+        this.value += variable;
+    }
+    public void odejmij(int variable)
+    {
+        this.value -= variable;
+    }
+    public void Wypisz()
+    {
+        Console.WriteLine("Liczba wynosi " + value);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace lab02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Licz liczba1 = new Licz(10);
+            Licz liczba2 = new Licz(30);
+            Licz liczba3 = new Licz(3);
+
+            liczba1.dodaj(10);
+            liczba2.odejmij(5);
+            liczba3.odejmij(1);
+
+            liczba1.Wypisz();
+            liczba2.Wypisz();
+            liczba3.Wypisz();
+        }
+    }
+}
